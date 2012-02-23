61b219ff864c869e6135d1540f19c61984bb4f7a
Greg Caporaso <gregcaporaso@gmail.com>
2012-02-23 03:57:05 +0000

    added functionality to predict metagenomes from otu table and genome table containing overlapping otu identifiers

--- picrust/predict_metagenomes.py
+++ picrust/predict_metagenomes.py
@@ -11,6 +11,35 @@ __maintainer__ = "Greg Caporaso"
 __email__ = "gregcaporaso@gmail.com"
 __status__ = "Development"
 
+from numpy import dot, array
+from biom.table import table_factory
 
-def predict_metagenomes(otu_table1,genome_table1):
-    raise NotImplementedError
+def predict_metagenomes(otu_table,genome_table):
+    """ predict metagenomes from otu table and genome table 
+    """
+    # identify the overlapping otus that can be used to predict metagenomes 
+    overlapping_otus = list(set(otu_table.ObservationIds) & 
+                            set(genome_table.SampleIds))
+    
+    if len(overlapping_otus) < 1:
+        raise ValueError,\
+         "No common OTUs between the otu table and the genome table, so can't predict metagenome."
+    
+    # create lists to contain filtered data - we're going to need the data in 
+    # numpy arrays, so it makes sense to compute this way rather than filtering
+    # the tables
+    otu_data = []
+    genome_data = []
+    
+    # build lists of filtered data
+    for obs_id in overlapping_otus:
+        otu_data.append(otu_table.observationData(obs_id))
+        genome_data.append(genome_table.sampleData(obs_id))
+    
+    # matrix multiplication to get the predicted metagenomes
+    new_data = dot(array(otu_data).T,array(genome_data)).T
+    
+    # return the result as a sparse biom table - the sample ids are now the 
+    # sample ids from the otu table, and the observation ids are now the 
+    # functions (i.e., observations) from the genome table
+    return table_factory(new_data,otu_table.SampleIds,genome_table.ObservationIds)
--- tests/test_predict_metagenomes.py
+++ tests/test_predict_metagenomes.py
@@ -14,6 +14,7 @@ __status__ = "Development"
 
 from cogent.util.unit_test import TestCase, main
 from biom.parse import parse_biom_table_str
+from biom.table import DenseTable
 from picrust.predict_metagenomes import predict_metagenomes
 
 class PredictMetagenomeTests(TestCase):
@@ -30,88 +31,9 @@ class PredictMetagenomeTests(TestCase):
         self.assertEqual(actual,self.predicted_metagenome_table1)
 
 
-otu_table1 = """{
-    "id":null,
-    "format": "Biological Observation Matrix v0.9",
-    "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html",
-    "type": "OTU table",
-    "generated_by": "QIIME revision XYZ",
-    "date": "2011-12-19T19:00:00",
-    "rows":[
-            {"id":"GG_OTU_1", "metadata":null},
-            {"id":"GG_OTU_2", "metadata":null},
-            {"id":"GG_OTU_3", "metadata":null},
-            {"id":"GG_OTU_4", "metadata":null},
-            {"id":"GG_OTU_5", "metadata":null}
-        ],
-    "columns": [
-            {"id":"Sample1", "metadata":null},
-            {"id":"Sample2", "metadata":null},
-            {"id":"Sample3", "metadata":null},
-            {"id":"Sample4", "metadata":null},
-            {"id":"Sample5", "metadata":null},
-            {"id":"Sample6", "metadata":null}
-        ],
-    "matrix_type": "sparse",
-    "matrix_element_type": "int",
-    "shape": [5, 6],
-    "data":[[0,2,1],
-            [1,0,5],
-            [1,1,1],
-            [1,3,2],
-            [1,4,3],
-            [1,5,1],
-            [2,2,1],
-            [2,3,4],
-            [2,4,2],
-            [3,0,2],
-            [3,1,1],
-            [3,2,1],
-            [3,5,1],
-            [4,1,1],
-            [4,2,1]
-           ]
-}"""
+otu_table1 = """{"rows": [{"id": "GG_OTU_1", "metadata": null}, {"id": "GG_OTU_2", "metadata": null}, {"id": "GG_OTU_3", "metadata": null}], "format": "Biological Observation Matrix v0.9", "data": [[0, 0, 1.0], [0, 1, 2.0], [0, 2, 3.0], [0, 3, 5.0], [1, 0, 5.0], [1, 1, 1.0], [1, 3, 2.0], [2, 2, 1.0], [2, 3, 4.0]], "columns": [{"id": "Sample1", "metadata": null}, {"id": "Sample2", "metadata": null}, {"id": "Sample3", "metadata": null}, {"id": "Sample4", "metadata": null}], "generated_by": "QIIME 1.4.0-dev, svn revision 2753", "matrix_type": "sparse", "shape": [3, 4], "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html", "date": "2012-02-22T20:50:05.024661", "type": "OTU table", "id": null, "matrix_element_type": "float"}"""
 
-genome_table1 = """{
-    "id":null,
-    "format": "Biological Observation Matrix v0.9",
-    "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html",
-    "type": "OTU table",
-    "generated_by": "QIIME revision XYZ",
-    "date": "2011-12-19T19:00:00",
-    "rows":[
-            {"id":"f1", "metadata":null},
-            {"id":"f2", "metadata":null},
-            {"id":"f3", "metadata":null},
-            {"id":"f4", "metadata":null}
-        ],
-    "columns": [
-            {"id":"Sample1", "metadata":null},
-            {"id":"Sample2", "metadata":null},
-            {"id":"Sample3", "metadata":null},
-            {"id":"Sample4", "metadata":null},
-            {"id":"Sample5", "metadata":null},
-            {"id":"Sample6", "metadata":null}
-        ],
-    "matrix_type": "sparse",
-    "matrix_element_type": "int",
-    "shape": [4, 6],
-    "data":[[0,2,1],
-            [1,0,5],
-            [1,1,1],
-            [1,3,2],
-            [1,4,3],
-            [1,5,1],
-            [2,2,1],
-            [2,3,4],
-            [2,4,2],
-            [3,0,2],
-            [3,1,1],
-            [3,2,1],
-            [3,5,1]
-           ]
-}"""
+genome_table1 = """{"rows": [{"id": "f1", "metadata": null}, {"id": "f2", "metadata": null}, {"id": "f3", "metadata": null}], "format": "Biological Observation Matrix v0.9", "data": [[0, 0, 1.0], [0, 1, 2.0], [0, 2, 3.0], [1, 1, 1.0], [2, 2, 1.0]], "columns": [{"id": "GG_OTU_1", "metadata": null}, {"id": "GG_OTU_3", "metadata": null}, {"id": "GG_OTU_2", "metadata": null}], "generated_by": "QIIME 1.4.0-dev, svn revision 2753", "matrix_type": "sparse", "shape": [3, 3], "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html", "date": "2012-02-22T20:49:58.258296", "type": "OTU table", "id": null, "matrix_element_type": "float"}"""
 
 predicted_metagenome_table1 = """{"rows": [{"id": "f1", "metadata": null}, {"id": "f2", "metadata": null}, {"id": "f3", "metadata": null}], "format": "Biological Observation Matrix v0.9", "data": [[0, 0, 16.0], [0, 1, 5.0], [0, 2, 5.0], [0, 3, 19.0], [1, 2, 1.0], [1, 3, 4.0], [2, 0, 5.0], [2, 1, 1.0], [2, 3, 2.0]], "columns": [{"id": "Sample1", "metadata": null}, {"id": "Sample2", "metadata": null}, {"id": "Sample3", "metadata": null}, {"id": "Sample4", "metadata": null}], "generated_by": "QIIME 1.4.0-dev, svn revision 2753", "matrix_type": "sparse", "shape": [3, 4], "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html", "date": "2012-02-22T16:01:30.837052", "type": "OTU table", "id": null, "matrix_element_type": "float"}"""
 
