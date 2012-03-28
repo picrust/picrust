732462675c06450d881712f1b49ea727718dc109
Jesse Zaneveld <zaneveld@gmail.com>
2012-03-28 23:31:23 +0000

    Test code for make_test_datasets.py

---
+++ tests/test_make_test_datasets.py
@@ -0,0 +1,171 @@
+from unittest import main, TestCase 
+from cogent import LoadTree
+from cogent.parse.tree import DndParser
+from picrust.make_test_datasets import exclude_tip, yield_test_trees,\
+  make_distance_based_exclusion_fn
+
+"""
+Tests for make_test_datasets.py
+"""
+
+__author__ = "Jesse Zaneveld"
+__copyright__ = "Copyright 2011, The PICRUST Project"
+__credits__ = ["Jesse Zaneveld"]
+__license__ = "GPL"
+__version__ = "0.4"
+__maintainer__ = "Jesse Zaneveld"
+__email__ = "zaneveld@gmail.com"
+__status__ = "Development"
+
+
+class TestMakeTestTrees(TestCase):
+    """Tests of make_test_trees.py"""
+
+    def setUp(self):
+        self.SimpleTree = \
+          DndParser("((A:0.02,B:0.01)E:0.05,(C:0.01,D:0.01)F:0.05)root;")
+    
+        self.SimplePolytomyTree = \
+                DndParser("((A:0.02,B:0.01,B_prime:0.03)E:0.05,(C:0.01,D:0.01)F:0.05)root;")
+    
+    def test_exclude_tip(self):
+        """exclude_tip should yield a holdout tree"""
+
+        #Try excluding tip 'B'
+        test_tree = self.SimpleTree.deepcopy()
+        
+        obs = exclude_tip(test_tree.getNodeMatchingName('B'),test_tree)
+        obs_newick = obs.getNewick(with_distances=True)
+        exp_newick = "((C:0.01,D:0.01)F:0.05,A:0.07)root;" 
+        self.assertEqual(obs_newick,exp_newick)
+        
+        #Make sure the holdout works with a polytomy
+        test_tree = self.SimplePolytomyTree.deepcopy()
+        
+        obs = exclude_tip(test_tree.getNodeMatchingName('B'),test_tree)
+        obs_newick = obs.getNewick(with_distances=True)
+        exp_newick = "((A:0.02,'B_prime':0.03)E:0.05,(C:0.01,D:0.01)F:0.05)root;" 
+        self.assertEqual(obs_newick,exp_newick)
+
+        #Make sure we raise if the tip is invalid        
+         
+        test_tree = self.SimpleTree.deepcopy()
+        
+        self.assertRaises(ValueError,exclude_tip,\
+          test_tree.getNodeMatchingName('E'),test_tree)
+
+    def test_make_distance_based_exclusion_fn(self):
+        """make_distance_based_exclusion_fn should return a working function"""
+
+        exclude_similar_strains =\
+            make_distance_based_exclusion_fn(0.03)
+        
+        #Test that the function works
+        
+        test_tree = self.SimpleTree.deepcopy()
+        #print test_tree.getNewick(with_distances=True)
+        tip = test_tree.getNodeMatchingName('C')
+        obs = exclude_similar_strains(tip,test_tree).getNewick(with_distances=True) 
+        exp = "((A:0.02,B:0.01)E:0.05,C:0.06)root;"
+        self.assertEqual(obs,exp)
+        #Test that new function is documented
+        exp_doc = 'Exclude neighbors of tip within 0.030000 branch length units'
+        self.assertEqual(exp_doc,exclude_similar_strains.__doc__)
+
+
+        #Test that we raise if distance is too large
+        test_tree = self.SimpleTree.deepcopy()
+        test_fn = make_distance_based_exclusion_fn(300.0)
+        tip = test_tree.getNodeMatchingName('C')
+        
+        self.assertRaises(ValueError,test_fn,tip,test_tree)
+
+
+    def make_distance_based_randomizer(self):
+        """make_distance_based_randomizer should randomize tip labels iwthin d"""
+
+        tip_randomizer =\
+            make_distance_based_tip_label_randomizer(0.08)
+
+        #Test that the function works
+
+        test_tree = self.SimpleTree.deepcopy()
+        print test_tree.asciiArt()
+        tip = test_tree.getNodeMatchingName('C')
+        obs = tip_randomizer(tip,test_tree).getNewick(with_distances=True) 
+        print "OBS:",obs.asciiArt()        
+        # TODO: finish this test!
+
+
+    def test_yield_test_trees(self):
+        """yield_test_trees should yield modified test trees"""
+        
+        start_tree = self.SimpleTree.deepcopy()
+
+
+        # First, test with simple tree and exclude_tip
+        # (not a typical use, but easy to predict
+        # and still important to test)
+
+        
+        test_fn = exclude_tip
+        obs = [o for o in yield_test_trees(start_tree, test_fn)]
+        test_tips =  [tip for tip,tree in obs]
+        test_trees = [tree for tip,tree in obs]
+        print test_tips
+        print test_trees 
+        #Test that each tree  excludes the correct tip
+        for i,exp_tip in enumerate(start_tree.tips()):
+            print exp_tip
+            node_names = [obs_tip.Name for obs_tip in test_trees[i].tips()]
+            print node_names
+            self.assertTrue(exp_tip.Name not in node_names)
+            
+        #Test that the topology is correct for an example tree 
+        self.assertEqual(test_trees[1].getNewick(with_distances=True),\
+            "((C:0.01,D:0.01)F:0.05,A:0.07)root;")
+        
+        #Second, let's test a more realistic test function
+        test_fn = make_distance_based_exclusion_fn(0.03)
+        
+        obs = [o for o in yield_test_trees(start_tree, test_fn)]
+        test_tips =  [tip for tip,tree in obs]
+        test_trees = [tree for tip,tree in obs]
+        #Test that each tree doesn't exclude the tip of interest  
+        for i,exp_tip in enumerate(start_tree.tips()):
+            print exp_tip
+            node_names = [obs_tip.Name for obs_tip in test_trees[i].tips()]
+            print node_names
+            self.assertTrue(exp_tip.Name in node_names)
+            
+        #Test that the topology is correct for all tips
+        
+        
+        #Tip 'A'
+        self.assertEqual(test_trees[0].getNewick(with_distances=True),\
+            "((C:0.01,D:0.01)F:0.05,A:0.07)root;")
+        
+        #Tip 'B'
+        self.assertEqual(test_trees[1].getNewick(with_distances=True),\
+            "((C:0.01,D:0.01)F:0.05,B:0.06)root;")
+        
+        #Tip 'C'
+        self.assertEqual(test_trees[2].getNewick(with_distances=True),\
+            "((A:0.02,B:0.01)E:0.05,C:0.06)root;")
+
+        #Tip 'D'
+        self.assertEqual(test_trees[3].getNewick(with_distances=True),\
+            "((A:0.02,B:0.01)E:0.05,D:0.06)root;")
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
