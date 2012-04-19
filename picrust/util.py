ef596e640091c8e3b5b3270e7dab75556f9d80c5
Morgan Langille <morgan.g.i.langille@gmail.com>
2012-04-19 15:12:36 +0000

    Move PicrustNode class to util module

--- picrust/util.py
+++ picrust/util.py
@@ -13,6 +13,7 @@ __status__ = "Development"
 
 from os.path import abspath, dirname, isdir
 from os import mkdir
+from cogent.core.tree import PhyloNode, TreeError
 
 def get_picrust_project_dir():
     """ Returns the top-level PICRUST directory
@@ -50,3 +51,142 @@ def make_output_dir(dirpath, strict=False):
 
     return dirpath
 
+
+class PicrustNode(PhyloNode):
+    def multifurcating(self, num, eps=None, constructor=None):
+        """Return a new tree with every node having num or few children
+
+        num : the number of children a node can have max
+        eps : default branch length to set if self or constructor is of
+            PhyloNode type
+        constructor : a TreeNode or subclass constructor. If None, uses self
+        """
+        if num < 2: 
+            raise TreeError, "Minimum number of children must be >= 2"
+
+        if eps is None:
+            eps = 0.0
+
+        if constructor is None:
+            constructor = self.__class__
+
+        if hasattr(constructor, 'Length'):
+            set_branchlength = True 
+        else:
+            set_branchlength = False
+
+        new_tree = self.copy()
+
+        for n in new_tree.preorder(include_self=True):
+            while len(n.Children) > num: 
+                new_node = constructor(Children=n.Children[-num:])
+
+                if set_branchlength:
+                    new_node.Length = eps
+
+                n.append(new_node)
+
+        return new_tree
+
+    def bifurcating(self, eps=None, constructor=None):
+        """Wrap multifurcating with a num of 2"""
+        return self.multifurcating(2, eps, constructor)
+
+    def getSubTree(self, name_list):
+        """A new instance of a sub tree that contains all the otus that are
+        listed in name_list.
+        just a small change from that in cogent.core.tree.py so that the root
+        node keeps its name
+        
+        Credit: Julia Goodrich
+        """
+        edge_names = self.getNodeNames(includeself=1, tipsonly=False)
+        for name in name_list:
+            if name not in edge_names:
+                raise ValueError("edge %s not found in tree" % name)
+        new_tree = self._getSubTree(name_list)
+        if new_tree is None:
+            raise TreeError, "no tree created in make sub tree"
+        elif new_tree.istip():
+            # don't keep name
+            new_tree.params = self.params
+            new_tree.Length = self.Length
+            return new_tree
+        else:
+            new_tree.Name = self.Name
+            new_tree.NameLoaded = self.NameLoaded
+            new_tree.params = self.params
+            new_tree.Length = self.Length
+            # keep unrooted
+            if len(self.Children) > 2:
+                new_tree = new_tree.unrooted()
+            return new_tree
+
+    def _getSubTree(self, included_names, constructor=None):
+        """An equivalent node with possibly fewer children, or None
+            this is an iterative version of that in cogent.core.tree.py
+
+        Credit: Julia Goodrich
+        """
+        nodes_stack = [[self, len(self.Children)]]
+        result = [[]]
+
+        # Renumber autonamed edges
+        if constructor is None:
+            constructor = self._default_tree_constructor()
+
+        while nodes_stack:
+            top = nodes_stack[-1]
+            top_node, num_unvisited_children = top
+            if top_node.Name in included_names:
+                result[-1].append(top_node.deepcopy(constructor=constructor))
+                nodes_stack.pop()
+            else:
+                #check the top node, any children left unvisited?
+                if num_unvisited_children: #has any child unvisited
+                    top[1] -= 1  #decrease the #of children unvisited
+                    next_child = top_node.Children[-num_unvisited_children]
+                    # - for order
+                    #pre-visit
+                    nodes_stack.append([next_child, len(next_child.Children)])
+                    if len(next_child.Children) > 0:
+                        result.append([])
+                else:
+                    node = nodes_stack.pop()[0]
+                    children = result[-1]
+                    children =[child for child in children if child is not None]
+                    if len(top_node.Children) == 0:
+                        new_node = None
+                    elif len(children) == 0:
+                        result.pop()
+                        new_node = None
+                    elif len(children) == 1:
+                        result.pop()
+                        # Merge parameter dictionaries by adding lengths and
+                        # making weighted averages of other parameters.  This
+                        # should probably be moved out of here into a
+                        # ParameterSet class (Model?) or tree subclass.
+                        params = {}
+                        child = children[0]
+
+                        if node.Length is not None and child.Length is not None:
+                            shared_params = [n for (n,v) in node.params.items()
+                                if v is not None
+                                and child.params.get(n) is not None
+                                and n is not "length"]
+                            length = node.Length + child.Length
+                            if length:
+                                params = dict([(n,
+                                        (node.params[n]*node.Length +
+                                        child.params[n]*child.Length) / length)
+                                    for n in shared_params])
+                            params['length'] = length
+                        new_node = child
+                        new_node.params = params
+                    else:
+                        result.pop()
+                        new_node = constructor(node, tuple(children))
+                    if len(result)>0:
+                        result[-1].append(new_node)
+                    else:
+                        return new_node
--- tests/test_util.py
+++ tests/test_util.py
@@ -18,6 +18,68 @@ from os.path import exists, dirname, abspath
 from unittest import TestCase, main
 from picrust.util import (get_picrust_project_dir,)
 
+from cogent.core.tree import TreeError
+from cogent.parse.tree import DndParser
+from picrust.util import PicrustNode
+
+class PicrustNodeTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_getsubtree(self):
+        """testing getting a subtree
+        
+        credit Julia Goodrich
+        """
+        otu_names = ['NineBande', 'Mouse', 'HowlerMon', 'DogFaced']
+        newick = '(((Human,HowlerMon),Mouse),NineBande,DogFaced);'
+        newick_reduced = '((HowlerMon,Mouse),NineBande,DogFaced);'
+        tree = DndParser(newick, constructor = PicrustNode) 
+
+        subtree = tree.unrooted().getSubTree(otu_names)
+        new_tree = DndParser(newick_reduced, constructor = PicrustNode).unrooted()       
+        # check we get the same names
+        self.assertEqual(*[len(t.Children) for t in (subtree,new_tree)])
+        self.assertEqual(subtree.getNewick(), new_tree.getNewick())
+
+
+    def test_multifurcating(self):
+        """Coerces nodes to have <= n children"""
+        t_str = "((a:1,b:2,c:3)d:4,(e:5,f:6,g:7)h:8,(i:9,j:10,k:11)l:12)m:14;"
+        t = DndParser(t_str)
+
+        # can't break up easily... sorry 80char
+        exp_str = "((a:1.0,(b:2.0,c:3.0):0.0)d:4.0,((e:5.0,(f:6.0,g:7.0):0.0)h:8.0,(i:9.0,(j:10.0,k:11.0):0.0)l:12.0):0.0)m:14.0;"
+        obs = t.multifurcating(2)
+        self.assertEqual(obs.getNewick(with_distances=True), exp_str)
+        self.assertNotEqual(t.getNewick(with_distances=True),
+                            obs.getNewick(with_distances=True))
+
+        obs = t.multifurcating(2, 0.5) 
+        exp_str = "((a:1.0,(b:2.0,c:3.0):0.5)d:4.0,((e:5.0,(f:6.0,g:7.0):0.5)h:8.0,(i:9.0,(j:10.0,k:11.0):0.5)l:12.0):0.5)m:14.0;"
+        self.assertEqual(obs.getNewick(with_distances=True), exp_str)
+
+        t_str = "((a,b,c)d,(e,f,g)h,(i,j,k)l)m;"
+        exp_str = "((a,(b,c))d,((e,(f,g))h,(i,(j,k))l))m;"
+        t = DndParser(t_str, constructor=PicrustNode)
+        obs = t.multifurcating(2)
+        self.assertEqual(obs.getNewick(with_distances=True), exp_str)
+        obs = t.multifurcating(2, eps=10) # no effect on TreeNode type
+        self.assertEqual(obs.getNewick(with_distances=True), exp_str)
+
+        self.assertRaises(TreeError, t.multifurcating, 1)
+
+    def test_bifurcating(self):
+        """Coerces nodes to have <= 2 children"""
+        t_str = "((a:1,b:2,c:3)d:4,(e:5,f:6,g:7)h:8,(i:9,j:10,k:11)l:12)m:14;"
+        t = DndParser(t_str)
+     
+        # can't break up easily... sorry 80char
+        exp_str = "((a:1.0,(b:2.0,c:3.0):0.0)d:4.0,((e:5.0,(f:6.0,g:7.0):0.0)h:8.0,(i:9.0,(j:10.0,k:11.0):0.0)l:12.0):0.0)m:14.0;"
+        obs = t.bifurcating()
+
+
+
 class UtilTests(TestCase):
     """ Tests of the picrust/util.py module """
     
