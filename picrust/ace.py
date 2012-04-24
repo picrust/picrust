fc6be2e8810677ae7d6cc4d62e7a213369d01ef1
Morgan Langille <morgan.g.i.langille@gmail.com>
2012-04-24 23:08:16 +0000

    Add reporting of stderr from ace.R if ace app controller fails

--- picrust/ace.py
+++ picrust/ace.py
@@ -53,7 +53,12 @@ def ace_for_picrust(tree_path,trait_table_path,method='pic',HALT_EXEC=False):
     result = ace(data=as_string)
 
     #Load the output into Table objects
-    asr_table=LoadTable(filename=tmp_output_count_path,header=True,sep='\t')
+    try:
+        asr_table=LoadTable(filename=tmp_output_count_path,header=True,sep='\t')
+    except IOError:
+        print "R wrote this to stderr:"
+        print "\n".join(result["StdErr"].readlines())
+    
     asr_prob_table=LoadTable(filename=tmp_output_prob_path,header=True,sep='\t')
 
     #Remove tmp files
