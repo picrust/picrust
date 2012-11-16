413a2dc3989880ab713ad3db85f4be0f8f60a039
Morgan Langille <morgan.g.i.langille@gmail.com>
2012-11-16 19:01:55 +0000

    Add extra options to compare_biom.py script including randomizing of samples

--- scripts/compare_biom.py
+++ scripts/compare_biom.py
@@ -21,6 +21,7 @@ from biom.parse import parse_biom_table, convert_biom_to_table
 from picrust.util import make_output_dir_for_file
 from biom.table import table_factory,DenseOTUTable,SparseOTUTable
 from picrust.parse import parse_trait_table, parse_marker_gene_copy_numbers
+from random import shuffle
 
 script_info = {}
 script_info['brief_description'] = "Compare the accuracy of biom files (expected and observed) either by observations (default) or by samples."
@@ -35,7 +36,10 @@ script_info['required_options'] = [
 script_info['optional_options'] = [
   make_option('-c','--compare_observations',action="store_true",default=False,help='Calculate accuracy values by comparing between observations (instead of between samples) [default: %default]'),\
   make_option('-n','--normalize',action="store_true",default=False,help='Convert both expected and observed tables to relative abundances (instead of observations) [default: %default]'),
-  make_option('-l','--limit_to_expected_observations',action="store_true",default=False,help='Ignore observations that are not in the expected table[default: %default]')
+  make_option('-l','--limit_to_expected_observations',action="store_true",default=False,help='Ignore observations that are not in the expected table[default: %default]'),
+  make_option('--limit_to_observed_observations',action="store_true",default=False,help='Ignore observations that are not in the observed table[default: %default]'),
+  make_option('-s','--shuffle_samples',action="store_true",default=False,help='Shuffle samples ids randomly before measuring accuracy[default: %default]'),
+  make_option('--not_relative_abundance_scores',action="store_true",default=False,help='Round numbers (instead of taking ceil() which is used for RA) before calculating TP,FP,FN,TN [default: %default]')
   
         ]
 script_info['disallow_positional_arguments'] = False
@@ -97,9 +101,10 @@ def match_biom_tables_by_observation(observed_table,expected_table_keep,verbose=
   #  return flat_obs_data,flat_exp_data
     return obs_data,exp_data
 
-def match_biom_tables_by_sample(observed_table,expected_table_keep,verbose=False,limit_to_expected_observations=False,normalize=False):
+def match_biom_tables_by_sample(observed_table,expected_table_keep,verbose=False,limit_to_expected_observations=False,limit_to_observed_observations=False,normalize=False,shuffle_samples=False):
    
     expected_table = expected_table_keep.copy()
+
     overlapping_obs_ids = list(set(observed_table.ObservationIds) &
                             set(expected_table.ObservationIds))
    
@@ -110,12 +115,16 @@ def match_biom_tables_by_sample(observed_table,expected_table_keep,verbose=False
         raise ValueError,\
          "No observation ids are in common  between the observed and expected tables, so no evaluations can be performed."
 
-    #import pdb; pdb.set_trace()
 
     if limit_to_expected_observations:
         def f(data_vector, id_, metadata):
             return (id_ in overlapping_obs_ids)
         observed_table=observed_table.filterObservations(f)
+
+    if limit_to_observed_observations:
+        def f(data_vector, id_, metadata):
+            return (id_ in overlapping_obs_ids)
+        expected_table=expected_table.filterObservations(f)
         
 
     ###Make tables have same set (e.g.number) of ObservationIds and in the same order###
@@ -146,6 +155,15 @@ def match_biom_tables_by_sample(observed_table,expected_table_keep,verbose=False
     overlapping_sample_ids = list(set(observed_table.SampleIds) &
                             set(expected_table.SampleIds))
 
+    if verbose:
+        num_uniq_obs_sample_ids=len(observed_table.SampleIds)-len(overlapping_sample_ids)
+        num_uniq_exp_sample_ids=len(expected_table.SampleIds)-len(overlapping_sample_ids)
+        if num_uniq_obs_sample_ids:
+            print "Num observed samples not in expected: {0}".format(num_uniq_obs_sample_ids) 
+        if num_uniq_exp_sample_ids:
+            print "Num expected samples not in observed: {0}".format(num_uniq_exp_sample_ids)
+        print "Num samples with same id: {0}".format(len(overlapping_sample_ids))
+
 
     if normalize:
         if verbose:
@@ -163,9 +181,22 @@ def match_biom_tables_by_sample(observed_table,expected_table_keep,verbose=False
     
     # build lists of filtered data
     for sample_id in overlapping_sample_ids:
-        obs_data[sample_id]=observed_table.sampleData(sample_id)
         exp_data[sample_id]=expected_table.sampleData(sample_id)
 
+    #import pdb; pdb.set_trace()
+
+    if shuffle_samples:
+        if verbose:
+            print "Randomly shufflying sample ids..."
+        sample_ids_to_shuffle=overlapping_sample_ids[:]
+        shuffle(sample_ids_to_shuffle)
+        
+        for index in range(len(overlapping_sample_ids)):
+            obs_data[overlapping_sample_ids[index]]=observed_table.sampleData(sample_ids_to_shuffle[index])
+    else:
+        for sample_id in overlapping_sample_ids:
+            obs_data[sample_id]=observed_table.sampleData(sample_id)
+        
     return obs_data,exp_data
         
 
@@ -209,7 +240,7 @@ def main():
         if opts.compare_observations:
             obs,exp=match_biom_tables_by_observation(obs_table,exp_table,verbose=verbose)
         else:
-            obs,exp=match_biom_tables_by_sample(obs_table,exp_table,verbose=verbose,limit_to_expected_observations=opts.limit_to_expected_observations,normalize=opts.normalize)
+            obs,exp=match_biom_tables_by_sample(obs_table,exp_table,verbose=verbose,limit_to_expected_observations=opts.limit_to_expected_observations,limit_to_observed_observations=opts.limit_to_observed_observations,normalize=opts.normalize,shuffle_samples=opts.shuffle_samples)
            
         if verbose:
             print "Calculating accuracy stats for all observations..."
@@ -218,7 +249,10 @@ def main():
         for i in obs:
             if verbose:
                 print "Calculating stats for: ",i
-            results=calculate_accuracy_stats_from_observations(obs[i],exp[i],success_criterion='ra_exact')
+            if opts.not_relative_abundance_scores:
+                results=calculate_accuracy_stats_from_observations(obs[i],exp[i],success_criterion='int_exact')
+            else:
+                results=calculate_accuracy_stats_from_observations(obs[i],exp[i],success_criterion='ra_exact')
 
             #If first pass then print out header
             if not header_printed:
