e607624e0c7424af315fa261e7626e01bcdcaad0
Jesse Zaneveld <zaneveld@gmail.com>
2012-11-29 01:18:31 +0000

    Updated test data to relect a change in how function x sample combinations with zero counts are handled

--- tests/test_predict_metagenomes.py
+++ tests/test_predict_metagenomes.py
@@ -66,6 +66,7 @@ class PredictMetagenomeTests(TestCase):
         #otu_data,genome_data,overlapping_ids = extract_otu_and_genome_data(self.otu_table1,self.genome_table1)
         #obs = partition_metagenome_contributions(otu_data,genome_data,self.otu_table1,self.genome_table1,overlapping_ids)
         obs_text = "\n".join(["\t".join(map(str,i)) for i in obs])
+        print obs_text
         exp_text = "\n".join(["\t".join(map(str,r.split())) for r in self.predicted_gene_partition_table.split('\n')])
 
         #Test that the percent of all samples is always smaller than the percent of the current sample
@@ -115,19 +116,20 @@ genome_table2 = """{"rows": [{"id": "f1", "metadata": null}, {"id": "f2", "metad
 
 predicted_metagenome_table1 = """{"rows": [{"id": "f1", "metadata": null}, {"id": "f2", "metadata": null}, {"id": "f3", "metadata": null}], "format": "Biological Observation Matrix v0.9", "data": [[0, 0, 16.0], [0, 1, 5.0], [0, 2, 5.0], [0, 3, 19.0], [1, 2, 1.0], [1, 3, 4.0], [2, 0, 5.0], [2, 1, 1.0], [2, 3, 2.0]], "columns": [{"id": "Sample1", "metadata": null}, {"id": "Sample2", "metadata": null}, {"id": "Sample3", "metadata": null}, {"id": "Sample4", "metadata": null}], "generated_by": "QIIME 1.4.0-dev, svn revision 2753", "matrix_type": "sparse", "shape": [3, 4], "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html", "date": "2012-02-22T16:01:30.837052", "type": "OTU table", "id": null, "matrix_element_type": "float"}"""
 
-predicted_gene_partition_table = """Gene    Sample  OTU GeneCountPerGenome  OTUAbundanceInSample    CountContributedByOTU   ContributionPercentOfSample ContributionPercentOfAllSamples
-f1  Sample1 GG_OTU_1    1.0 1.0 1.0 0.0625  0.022222217284
-f1  Sample1 GG_OTU_2    3.0 5.0 15.0    0.9375  0.333333259259
+predicted_gene_partition_table =\
+ """Gene    Sample  OTU GeneCountPerGenome  OTUAbundanceInSample    CountContributedByOTU   ContributionPercentOfSample ContributionPercentOfAllSamples
+f1  Sample1 GG_OTU_1    1.0 1.0 1.0 0.0625  0.0222222222222
+f1  Sample1 GG_OTU_2    3.0 5.0 15.0    0.9375  0.333333333333
 f1  Sample1 GG_OTU_3    2.0 0.0 0.0 0.0 0.0
-f1  Sample2 GG_OTU_1    1.0 2.0 2.0 0.4 0.0444444345679
-f1  Sample2 GG_OTU_2    3.0 1.0 3.0 0.6 0.0666666518519
+f1  Sample2 GG_OTU_1    1.0 2.0 2.0 0.4 0.0444444444444
+f1  Sample2 GG_OTU_2    3.0 1.0 3.0 0.6 0.0666666666667
 f1  Sample2 GG_OTU_3    2.0 0.0 0.0 0.0 0.0
-f1  Sample3 GG_OTU_1    1.0 3.0 3.0 0.6 0.0666666518519
+f1  Sample3 GG_OTU_1    1.0 3.0 3.0 0.6 0.0666666666667
 f1  Sample3 GG_OTU_2    3.0 0.0 0.0 0.0 0.0
-f1  Sample3 GG_OTU_3    2.0 1.0 2.0 0.4 0.0444444345679
-f1  Sample4 GG_OTU_1    1.0 5.0 5.0 0.263157894737  0.11111108642
-f1  Sample4 GG_OTU_2    3.0 2.0 6.0 0.315789473684  0.133333303704
-f1  Sample4 GG_OTU_3    2.0 4.0 8.0 0.421052631579  0.177777738272
+f1  Sample3 GG_OTU_3    2.0 1.0 2.0 0.4 0.0444444444444
+f1  Sample4 GG_OTU_1    1.0 5.0 5.0 0.263157894737  0.111111111111
+f1  Sample4 GG_OTU_2    3.0 2.0 6.0 0.315789473684  0.133333333333
+f1  Sample4 GG_OTU_3    2.0 4.0 8.0 0.421052631579  0.177777777778
 f2  Sample1 GG_OTU_1    0.0 1.0 0.0 0.0 0.0
 f2  Sample1 GG_OTU_2    0.0 5.0 0.0 0.0 0.0
 f2  Sample1 GG_OTU_3    1.0 0.0 0.0 0.0 0.0
@@ -136,23 +138,22 @@ f2  Sample2 GG_OTU_2    0.0 1.0 0.0 0.0 0.0
 f2  Sample2 GG_OTU_3    1.0 0.0 0.0 0.0 0.0
 f2  Sample3 GG_OTU_1    0.0 3.0 0.0 0.0 0.0
 f2  Sample3 GG_OTU_2    0.0 0.0 0.0 0.0 0.0
-f2  Sample3 GG_OTU_3    1.0 1.0 1.0 1.0 0.199999600001
+f2  Sample3 GG_OTU_3    1.0 1.0 1.0 1.0 0.2
 f2  Sample4 GG_OTU_1    0.0 5.0 0.0 0.0 0.0
 f2  Sample4 GG_OTU_2    0.0 2.0 0.0 0.0 0.0
-f2  Sample4 GG_OTU_3    1.0 4.0 4.0 1.0 0.799998400003
+f2  Sample4 GG_OTU_3    1.0 4.0 4.0 1.0 0.8
 f3  Sample1 GG_OTU_1    0.0 1.0 0.0 0.0 0.0
-f3  Sample1 GG_OTU_2    1.0 5.0 5.0 1.0 0.624999218751
+f3  Sample1 GG_OTU_2    1.0 5.0 5.0 1.0 0.625
 f3  Sample1 GG_OTU_3    0.0 0.0 0.0 0.0 0.0
 f3  Sample2 GG_OTU_1    0.0 2.0 0.0 0.0 0.0
-f3  Sample2 GG_OTU_2    1.0 1.0 1.0 1.0 0.12499984375
+f3  Sample2 GG_OTU_2    1.0 1.0 1.0 1.0 0.125
 f3  Sample2 GG_OTU_3    0.0 0.0 0.0 0.0 0.0
 f3  Sample3 GG_OTU_1    0.0 3.0 0.0 0.0 0.0
 f3  Sample3 GG_OTU_2    1.0 0.0 0.0 0.0 0.0
 f3  Sample3 GG_OTU_3    0.0 1.0 0.0 0.0 0.0
 f3  Sample4 GG_OTU_1    0.0 5.0 0.0 0.0 0.0
-f3  Sample4 GG_OTU_2    1.0 2.0 2.0 1.0 0.2499996875
+f3  Sample4 GG_OTU_2    1.0 2.0 2.0 1.0 0.25
 f3  Sample4 GG_OTU_3    0.0 4.0 0.0 0.0 0.0"""
- 
 
 if __name__ == "__main__":
     main()
