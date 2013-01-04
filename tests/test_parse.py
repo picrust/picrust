241c72f9ab74daae10dd8b48c9b72bd5c649f237
mlangill <morgan.g.i.langille@gmail.com>
2013-01-03 22:39:26 -0400

    Opted to remove print statements instead of commenting

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -24,25 +24,6 @@ class ParseTests(TestCase):
         self.counts_bad1 = counts_bad1.split('\n')
         self.counts_bad2 = counts_bad2.split('\n')
     
-#    def test_parse_marker_gene_copy_numbers(self):
-#        """ parse_marker_gene_copy_numbers functions as expected with valid input 
-#        """
-        
-#        actual = parse_marker_gene_copy_numbers(self.counts_f1,
-#                                                metadata_identifier='cn')
-#        expected = {'GG_OTU_1':{'cn':2},
-#                'GG_OTU_2':{'cn':4},
-#                'GG_OTU_3':{'cn':1}}
-#        self.assertEqual(actual,expected)
-    
-#        def test_parse_marker_gene_copy_numbers_invalid(self):
-#            """ parse_marker_gene_copy_numbers raises ValueError on invalid input 
-#            """
-#            self.assertRaises(ValueError,parse_marker_gene_copy_numbers,self.counts_bad1,'cn')
-#            self.assertRaises(ValueError,parse_marker_gene_copy_numbers,self.counts_bad2,'cn')
-
-
-    
     def test_extract_ids_from_table_qiime_legacy_otu_table(self):
         """extract_ids_from_table extracts ids from a legacy QIIME 1.3 OTU table
         """
