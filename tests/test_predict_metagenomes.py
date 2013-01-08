de1619f6248ce6e92ff0e1438e2d9ed54816b578
zaneveld <zaneveld@gmail.com>
2013-01-08 15:34:41 -0800

    Added tests for transfer_sample_metadata and transfer_observation_metadata

--- tests/test_predict_metagenomes.py
+++ tests/test_predict_metagenomes.py
@@ -17,7 +17,8 @@ from biom.parse import parse_biom_table_str
 from biom.table import DenseTable
 from picrust.predict_metagenomes import predict_metagenomes,\
   calc_nsti,get_overlapping_ids,\
-  extract_otu_and_genome_data
+  extract_otu_and_genome_data,transfer_sample_metadata,\
+  transfer_observation_metadata,transfer_metadata
 
 class PredictMetagenomeTests(TestCase):
     """ """
@@ -60,7 +61,8 @@ class PredictMetagenomeTests(TestCase):
         """predict_metagenomes preserves Sample metadata in genome and otu table"""
         #NOTE: could be consolidated with "_keeps_observation_metadata above
 
-        actual = predict_metagenomes(self.otu_table1_with_metadata,self.genome_table1_with_metadata)
+        actual = predict_metagenomes(self.otu_table1_with_metadata,\
+          self.genome_table1_with_metadata,verbose=False)
         exp = self.predicted_metagenome_table1_with_metadata
         
         #Need to map to dicts, otherwise the memory location of the lambda function
@@ -73,7 +75,64 @@ class PredictMetagenomeTests(TestCase):
         for i,md in enumerate(exp_md):
             self.assertEqualItems(md,actual_md[i])
 
-   
+    def test_transfer_metadata_moves_sample_metadata_between_biom_tables(self):
+        """transfer_metadata moves sample metadata values between BIOM format tables"""
+        t1 = self.otu_table1
+        exp = self.otu_table1_with_metadata
+        actual = transfer_metadata(self.otu_table1_with_metadata,self.otu_table1,\
+          "SampleMetadata","SampleMetadata",verbose=False)
+        
+        actual_md = map(dict,sorted([md for md in actual.SampleMetadata]))
+        exp_md = map(dict,sorted([md for md in exp.SampleMetadata]))
+        for i,md in enumerate(actual_md):
+            self.assertEqualItems(md,exp_md[i])
+        for i,md in enumerate(exp_md):
+            self.assertEqualItems(md,actual_md[i])
+    
+    def test_transfer_metadata_moves_observation_metadata_between_biom_tables(self):
+        """transfer_metadata moves observation metadata values between BIOM format tables"""
+        t1 = self.genome_table1
+        exp = self.genome_table1_with_metadata
+        actual = transfer_metadata(self.genome_table1_with_metadata,\
+          self.genome_table1,"ObservationMetadata","ObservationMetadata",verbose=False)
+        
+        actual_md = map(dict,sorted([md for md in actual.ObservationMetadata]))
+        exp_md = map(dict,sorted([md for md in exp.ObservationMetadata]))
+        for i,md in enumerate(actual_md):
+            self.assertEqualItems(md,exp_md[i])
+        for i,md in enumerate(exp_md):
+            self.assertEqualItems(md,actual_md[i])
+
+
+    def test_transfer_sample_metadata_moves_sample_metadata_between_biom_tables(self):
+        """transfer_sample_metadata moves sample metadata values between BIOM format tables"""
+        t1 = self.otu_table1
+        exp = self.otu_table1_with_metadata
+        actual = transfer_sample_metadata(self.otu_table1_with_metadata,\
+          self.otu_table1,"SampleMetadata",verbose=False)
+        
+        actual_md = map(dict,sorted([md for md in actual.SampleMetadata]))
+        exp_md = map(dict,sorted([md for md in exp.SampleMetadata]))
+        for i,md in enumerate(actual_md):
+            self.assertEqualItems(md,exp_md[i])
+        for i,md in enumerate(exp_md):
+            self.assertEqualItems(md,actual_md[i])
+
+    def test_transfer_observation_metadata_moves_observation_metadata_between_biom_tables(self):
+        """transfer_sample_metadata moves sample metadata values between BIOM format tables"""
+        t1 = self.genome_table1
+        exp = self.genome_table1_with_metadata
+        actual = transfer_observation_metadata(self.genome_table1_with_metadata,\
+          self.genome_table1,"ObservationMetadata",verbose=False)
+        
+        actual_md = map(dict,sorted([md for md in actual.ObservationMetadata]))
+        exp_md = map(dict,sorted([md for md in exp.ObservationMetadata]))
+        for i,md in enumerate(actual_md):
+            self.assertEqualItems(md,exp_md[i])
+        for i,md in enumerate(exp_md):
+            self.assertEqualItems(md,actual_md[i])
+
+
 
 otu_table1 = """{"rows": [{"id": "GG_OTU_1", "metadata": null}, {"id": "GG_OTU_2", "metadata": null}, {"id": "GG_OTU_3", "metadata": null}], "format": "Biological Observation Matrix v0.9", "data": [[0, 0, 1.0], [0, 1, 2.0], [0, 2, 3.0], [0, 3, 5.0], [1, 0, 5.0], [1, 1, 1.0], [1, 3, 2.0], [2, 2, 1.0], [2, 3, 4.0]], "columns": [{"id": "Sample1", "metadata": null}, {"id": "Sample2", "metadata": null}, {"id": "Sample3", "metadata": null}, {"id": "Sample4", "metadata": null}], "generated_by": "QIIME 1.4.0-dev, svn revision 2753", "matrix_type": "sparse", "shape": [3, 4], "format_url": "http://www.qiime.org/svn_documentation/documentation/biom_format.html", "date": "2012-02-22T20:50:05.024661", "type": "OTU table", "id": null, "matrix_element_type": "float"}"""
 
