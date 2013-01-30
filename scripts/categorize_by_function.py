2fcd6f7f5d17bac78a1aaca950edeeacc7f26986
Daniel McDonald <mcdonadt@colorado.edu>
2013-01-30 15:49:08 -0700

    added ability to ignore other/unclassified

--- scripts/categorize_by_function.py
+++ scripts/categorize_by_function.py
@@ -25,12 +25,16 @@ script_info['required_options'] = [\
  make_option('-c','--metadata_category',type='string',help='the metadata category that describes the hierarchy'),
  make_option('-l','--level',type='int',help='the level in the hierarchy to collapse to. A value of 0 is not allowed, a valuue of 1 is the highest level, and any higher value nears the leaves of the hierarchy. For instance, if the hierarchy contains 4 levels, specifying 3 would collapse at one level above being fully specified.')
 ]
-script_info['optional_options'] = []
+script_info['optional_options'] = [
+ make_option('--collapse_unknonwn',action='store_true',default=False, help="Collapse categories that are described as 'other' or 'unclassified.' The default behavior is to ignore these groups [default: %default]")]
 script_info['version'] = __version__
 
-def make_collapse_f(category, level):
+def make_collapse_f(category, level, ignore_unknown):
     def collapse(md):
+        ignore_labels = ['unclassified', 'other']
         for path in md[category]:
+            if ignore_unknown and path[level].lower() in ignore_labels:
+                continue
             yield (path[:(level+1)],path[level])
     return collapse
 
@@ -41,7 +45,8 @@ def main():
     if opts.level <= 0:
         parser.error("level must be greater than zero!")
 
-    collapse_f = make_collapse_f(opts.metadata_category, opts.level)
+    collapse_f = make_collapse_f(opts.metadata_category, opts.level, 
+                                 opts.collapse_unknown)
     table = parse_biom_table(open(opts.input_fp))
     result = table.collapseObservationsByMetadata(collapse_f, one_to_many=True, 
                                                   norm=False)
