849378174f5ce3b9528331297feb96752c79be39
Daniel McDonald <mcdonadt@colorado.edu>
2013-01-30 16:13:02 -0700

    allow user to specify labels to ignore via cmdline

--- scripts/categorize_by_function.py
+++ scripts/categorize_by_function.py
@@ -26,14 +26,18 @@ script_info['required_options'] = [\
  make_option('-l','--level',type='int',help='the level in the hierarchy to collapse to. A value of 0 is not allowed, a valuue of 1 is the highest level, and any higher value nears the leaves of the hierarchy. For instance, if the hierarchy contains 4 levels, specifying 3 would collapse at one level above being fully specified.')
 ]
 script_info['optional_options'] = [
- make_option('--collapse_unknonwn',action='store_true',default=False, help="Collapse categories that are described as 'other' or 'unclassified.' The default behavior is to ignore these groups [default: %default]")]
+ make_option('--ignore',type='string',default=None, help="Ignore the comma separated list of names. For instance, specifying --ignore_unknown=unknown,unclassified will ignore those labels while collapsing. The default is to not ignore anything. [default: %default]")]
 script_info['version'] = __version__
 
-def make_collapse_f(category, level, ignore_unknown):
+def make_collapse_f(category, level, ignore):
     def collapse(md):
-        ignore_labels = ['unclassified', 'other']
+        if ignore:
+            ignore_labels = set(ignore)
+        else:
+            ignore_lables = None
+
         for path in md[category]:
-            if ignore_unknown and path[level].lower() in ignore_labels:
+            if ignore is not None and path[level].lower() in ignore_labels:
                 continue
             yield (path[:(level+1)],path[level])
     return collapse
@@ -46,7 +50,7 @@ def main():
         parser.error("level must be greater than zero!")
 
     collapse_f = make_collapse_f(opts.metadata_category, opts.level, 
-                                 opts.collapse_unknown)
+                                 opts.ignore.split(','))
     table = parse_biom_table(open(opts.input_fp))
     result = table.collapseObservationsByMetadata(collapse_f, one_to_many=True, 
                                                   norm=False)
