558d300bb4da33cf45858a9f7fda7e9710c51c68
Morgan Langille <morgan.g.i.langille@gmail.com>
2013-03-22 15:15:02 -0300

    Add __version__ and other global variables

--- picrust/__init__.py
+++ picrust/__init__.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python
+
+__author__ = "The PICRUSt Development Team"
+__copyright__ = "Copyright 2012, BIOM-Format Project"
+__credits__ = ["Morgan Langille","Jesse Zaneveld", "Greg Caporaso","Daniel McDonald", 
+               "Dan Knights","Joshua Reyes","Jose Clemente","Rob Knight","Rob Beiko","Curtis Huttenhower"]
+__license__ = "GPL"
+__url__ = "http://picrust.github.com"
+__version__ = "0.9.1-dev"
+__maintainer__ = "Morgan Langille"
+__email__ = "morgan.g.i.langille@gmail.com"
 
 
