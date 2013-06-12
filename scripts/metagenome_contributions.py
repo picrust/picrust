613e556a9d16d042310f2a63d1cb7b70f0fabfe0
Morgan Langille <morgan.g.i.langille@gmail.com>
2013-06-12 13:56:35 -0300

    Fix up metagenome_contributions.py
    Remove unneeded options and update usage statements.
    Also, update the selection of the precalculated file so it is
    similar to predict_metagenomes.py.

--- scripts/metagenome_contributions.py
+++ scripts/metagenome_contributions.py
@@ -17,26 +17,35 @@ from cogent.util.option_parsing import parse_command_line_parameters, make_optio
 from biom.parse import parse_biom_table
 from picrust.predict_metagenomes import predict_metagenomes, calc_nsti
 from picrust.metagenome_contributions import partition_metagenome_contributions
-from picrust.util import make_output_dir_for_file
+from picrust.util import make_output_dir_for_file, get_picrust_project_dir
 from os import path
+from os.path import join
 import gzip
 
 script_info = {}
 script_info['brief_description'] = "This script partitions metagenome functional contributions according to function, OTU, and sample, for a given OTU table."
 script_info['script_description'] = ""
-script_info['script_usage'] = [("","Partition the predicted contribution to the  metagenomes from each organism in otus.biom, using the predicted genes for each organism in genes.biom.","%prog -i otus.biom -c KEGG_acepic__predict_traits_97.biom.gz -o predicted_metagenomes.biom"),
-                               ("","Change output format to plain tab-delimited:","%prog -f -i otus.biom -c KEGG_acepic_predict_traits_97.biom.gz -o predicted_metagenomes.tab")]
-script_info['output_description']= "Output is a table of function counts (e.g. KEGG KOs) by sample ids."
+script_info['script_usage'] = [
+("","Partition the predicted contribution to the  metagenomes from each organism in the given OTU table, limited to only K00001, K00002, and K00004.","%prog -i normalized_otus.biom -l K00001,K00002,K00004 -o ko_metagenome_contributions.tab"),
+("","Partition the predicted contribution to the  metagenomes from each organism in the given OTU table, limited to only COG0001 and COG0002.","%prog -i normalized_otus.biom -l COG0001,COG0002 -t COG -o cog_metagenome_contributions.tab")
+]
+script_info['output_description']= "Output is a tab-delimited column indicating OTU contribution to each function."
 script_info['required_options'] = [
  make_option('-i','--input_otu_table',type='existing_filepath',help='the input otu table in biom format'),
- make_option('-c','--input_count_table',type="existing_filepath",help='the input trait counts on per otu basis in biom format (can be gzipped)'),
- make_option('-o','--output_metagenome_table',type="new_filepath",help='the output file for the predicted metagenome')
+ make_option('-o','--output_fp',type="new_filepath",help='the output file for the metagenome contributions')
 ]
-script_info['optional_options'] = [\
-        make_option('-a','--accuracy_metrics',default=None,type="new_filepath",help='If provided, calculate accuracy metrics for the predicted metagenome.  NOTE: requires that per-genome accuracy metrics were calculated using predict_traits.py during genome prediction (e.g. there are "NSTI" values in the genome .biom file metadata)'), 
-        
-        make_option('--limit_to_function',default=None,help='If provided, only output predictions for the specified function ids.  Multiple function ids can be passed using comma delimiters.'),
-        make_option('-f','--format_tab_delimited',action="store_true",default=False,help='output the predicted metagenome table in tab-delimited format [default: %default]')]
+type_of_prediction_choices=['KO','COG']
+
+script_info['optional_options'] = [
+        make_option('-t','--type_of_prediction',default='KO',type="choice",\
+                    choices=type_of_prediction_choices,\
+                    help='Type of functional predictions. Valid choices are: '+\
+                    ', '.join(type_of_prediction_choices)+\
+                    ' [default: %default]'),
+        make_option('-c','--input_count_table',default=None,type="existing_filepath",help='Precalculated function predictions on per otu basis in biom format (can be gzipped). Note: using this option overrides --type_of_prediction. [default: %default]'),
+ 
+        make_option('-l','--limit_to_function',default=None,help='If provided, only output predictions for the specified function ids.  Multiple function ids can be passed using comma delimiters.')
+]
 script_info['version'] = __version__
 
 
@@ -56,24 +65,36 @@ def main():
         print "Loading otu table: ",opts.input_otu_table
 
     otu_table = parse_biom_table(open(opts.input_otu_table,'U'))
-    ext=path.splitext(opts.input_count_table)[1]
+
+    if(opts.input_count_table is None):
+        if(opts.type_of_prediction == 'KO'):
+            input_count_table=join(get_picrust_project_dir(),'picrust','data','ko_precalculated.biom.gz')
+        elif(opts.type_of_prediction == 'COG'):
+            input_count_table=join(get_picrust_project_dir(),'picrust','data','cog_precalculated.biom.gz')
+    else:
+        input_count_table=opts.input_count_table
 
     if opts.verbose:
-        print "Loading count table: ", opts.input_count_table
+        print "Loading trait table: ", input_count_table
+
+    ext=path.splitext(input_count_table)[1]
+
+    if opts.verbose:
+        print "Loading count table: ", input_count_table
     if (ext == '.gz'):
-        genome_table = parse_biom_table(gzip.open(opts.input_count_table,'rb'))
+        genome_table = parse_biom_table(gzip.open(input_count_table,'rb'))
     else:
-        genome_table = parse_biom_table(open(opts.input_count_table,'U'))
+        genome_table = parse_biom_table(open(input_count_table,'U'))
     if opts.verbose:
         print "Predicting the metagenome..."
     
     partitioned_metagenomes = partition_metagenome_contributions(otu_table,genome_table,limit_to_functions=limit_to_functions)
     output_text = "\n".join(["\t".join(map(str,i)) for i in partitioned_metagenomes])
     if opts.verbose:
-        print "Writing results to output file: ",opts.output_metagenome_table
+        print "Writing results to output file: ",opts.output_fp
         
-    make_output_dir_for_file(opts.output_metagenome_table)
-    open(opts.output_metagenome_table,'w').write(output_text)
+    make_output_dir_for_file(opts.output_fp)
+    open(opts.output_fp,'w').write(output_text)
 
 if __name__ == "__main__":
     main()
