ef2f9c3ce3a6b75a63f7604eaea6423dbd11c7fb
Daniel McDonald <mcdonadt@colorado.edu>
2015-03-24 16:19:48 -0600

    REL: bumping minimum required biom version to 2.1.4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(name='PICRUSt',
       scripts=glob('scripts/*py'),
       install_requires=['numpy >= 1.5.1',
                         'cogent == 1.5.3',
-                        'biom-format >= 2.1.2, < 2.2.0'],
+                        'biom-format >= 2.1.4, < 2.2.0'],
       package_data={'picrust':
                     ['data/*gz',
                      'support_files/jar/Count.jar',
